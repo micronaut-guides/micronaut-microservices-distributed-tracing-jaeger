d74f69ea96d584c4f2ff3692e04d59f84ac7b945
sdelamo <sergio.delamo@softamo.com>
2018-08-21 11:17:11 +0200

    Use map().orElse with optional

--- complete/bookinventory/src/main/java/example/micronaut/bookinventory/BooksController.java
+++ complete/bookinventory/src/main/java/example/micronaut/bookinventory/BooksController.java
@@ -18,12 +18,7 @@ public class BooksController {
     @Get("/stock/{isbn}")
     @ContinueSpan // <1>
     public Boolean stock(@SpanTag("stock.isbn") @NotBlank String isbn) { // <2>
-        Optional<BookInventory> bookInventoryOptional = bookInventoryByIsbn(isbn);
-        if (!bookInventoryOptional.isPresent()) {
-            return null;
-        }
-        BookInventory bookInventory = bookInventoryOptional.get();
-        return bookInventory.getStock() > 0 ? Boolean.TRUE : Boolean.FALSE;
+        return bookInventoryByIsbn(isbn).map(bi -> bi.getStock() > 0 ? Boolean.TRUE : Boolean.FALSE).orElse(null);
     }
 
     private Optional<BookInventory> bookInventoryByIsbn(String isbn) {
